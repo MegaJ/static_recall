6afa7267b0ded1dcce530c376faed0ea1c571c46
Welile Zwane <wzwane@macalester.edu>
2016-05-07 20:35:23 -0500

    removed unused import

--- app/src/main/java/yiwejeje/staticrecallapp/Activity/AboutScreenActivity.java
+++ app/src/main/java/yiwejeje/staticrecallapp/Activity/AboutScreenActivity.java
@@ -3,7 +3,6 @@ package yiwejeje.staticrecallapp.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.WindowManager;
 
 import yiwejeje.staticrecallapp.R;
 
